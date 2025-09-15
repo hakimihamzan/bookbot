474b6329f1659ce06d09448825be44aa5fd5fbcd
hakimihamzan <hakimihamzan20@gmail.com>
2025-09-15 23:55:30 +0800

    add count_words function to stats.py and update main.py to use it; update .gitignore to include __pycache__

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
+from stats import count_words
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-
-
-def count_words(text):
-    res = text.split()
-    num_words = len(res)
-
-    return num_words
 
 
 def main():
---
+++ stats.py
@@ -0,0 +1,5 @@
+def count_words(text):
+    res = text.split()
+    num_words = len(res)
+
+    return num_words
