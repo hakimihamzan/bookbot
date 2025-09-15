6af8a2b8a7ed13d92fa641af908903e1e4669120
hakimihamzan <hakimihamzan20@gmail.com>
2025-09-15 23:47:09 +0800

    implement book text retrieval and update main function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+
+def main():
+    res = get_book_text("./books/frankenstein.txt")
+
+    print(res)
+
+
+main()
