cda9f6c135d502e51ec668822e59fd6d3ed6a94c
hakimihamzan <hakimihamzan20@gmail.com>
2025-09-15 23:53:07 +0800

    refactor main function to include word count and improve output

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ def get_book_text(filepath):
         return f.read()
 
 
+def count_words(text):
+    res = text.split()
+    num_words = len(res)
+
+    return num_words
+
+
 def main():
     res = get_book_text("./books/frankenstein.txt")
+    num_words = count_words(res)
 
-    print(res)
+    print(f"{num_words} words found in the document")
 
 
 main()
